1dd264dbef1e12d8cd34ea8cb8488cabdc45ac6c
medhat <medhat.mebed@gmail.com>
2018-02-11 08:45:09 +0200

    Initial Commit

---
+++ pixelCity/Contoller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pixelCity
+//
+//  Created by medhat on 2/11/18.
+//  Copyright © 2018 Medhatm3bd. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pixelCity/Model/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  pixelCity
+//
+//  Created by medhat on 2/11/18.
+//  Copyright © 2018 Medhatm3bd. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "903fb47b981b71bcaa600362da5fe8a2"
+
+func flickrUrl(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String {
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+}
